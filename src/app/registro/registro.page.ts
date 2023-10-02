80303500a1b79e0e1f37b1b2da7dd3e38fc991ef
ML OK <m.larenasnova@duocuc.cl>
2023-10-02 14:41:51 -0400

    Fin vista login y registro, validacion registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +14,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  formularioRegistro: FormGroup;
+
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController) { 
+    this.formularioRegistro = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required),
+      'confirmacionPassword': new FormControl("", Validators.required)
+    })
+
+  }
 
   ngOnInit() {
+  }
+
+  async guardar(){
+    var f = this.formularioRegistro.value;
+
+    if(this.formularioRegistro.invalid){
+      const alert = await this.alertController.create({
+        message: 'Tienes que llenar todos los datos',
+        buttons: ['Aceptar']
+      });
+
+    await alert.present();
+    return;
+    }
   }
 
 }
